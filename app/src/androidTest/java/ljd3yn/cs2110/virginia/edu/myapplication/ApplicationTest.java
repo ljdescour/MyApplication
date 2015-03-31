ee39b94d77815421c038dd4002acd78be3bd91f4
ljdescour <ljdescour@gmail.com>
2015-03-31 00:00:35 -0400

    TEST

---
+++ app/src/androidTest/java/ljd3yn/cs2110/virginia/edu/myapplication/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ljd3yn.cs2110.virginia.edu.myapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
